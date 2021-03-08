613349330dc763ae944c89e934040954eefa8f16
Martín Pérez <jose.martin@mercadolibre.com>
2021-03-08 12:44:37 -0300

    Resolve conflicts

--- demoapp/src/main/java/com/mercadolibre/android/andesui/demoapp/feature/DatePickerShowcaseActivity.kt
+++ demoapp/src/main/java/com/mercadolibre/android/andesui/demoapp/feature/DatePickerShowcaseActivity.kt
@@ -16,6 +16,7 @@ import java.text.DateFormat
 import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.Date
+import java.util.Calendar
 
 class DatePickerShowcaseActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
